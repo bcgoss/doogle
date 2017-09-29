a2e5230553cb078ba8d62493e97c87e1071d2f93
Bryan Goss <instantink+pair+bryan@hp.com>
2017-09-29 11:37:51 -0700

    Switch to Rails Admin

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,12 @@ Rails.application.routes.draw do
   root to: 'searches#show'
   resources :searches, only: [:new, :create, :index, :show]
 
-  get '/signup', to: 'users#new', as: 'signup'
   resources :users, only: [:create, :show]
 
   devise_scope :user do
+    get '/signup', to: 'devise/registration#new', as: 'signup'
     get '/login', to: 'devise/sessions#new', as: 'login'
     post '/login', to: 'devise/sessions#create'
     get '/logout', to: 'devise/sessions#destroy', as: 'logout'
-  end
-
-  namespace :admin do
-    resources :entries
-    resources :definitions
-    # get '/', to: 'dashboard#index', as: 'dashboard'
   end
 end
--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -1,52 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe SearchesController do
-  context 'guest' do
-    it 'can see Show' do
-      get :show
-      expect(response).to have_http_status(:success)
+  describe 'permissions' do
+    context 'guest' do
+      it 'can see Show' do
+        get :show
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'can see Create' do
+        entry = create :entry
+        post :create, params: { entry: { word: entry.word }, format: 'js'}
+        expect(response).to have_http_status(:success)
+      end
     end
 
-    it 'can see Create' do
-      entry = create :entry
-      post :create, params: { entry: { word: entry.word }, format: 'js'}
-      expect(response).to have_http_status(:success)
+    context 'user' do
+      before(:each) do
+        @user = create :user
+        sign_in @user
+      end
+
+      it 'can see Show' do
+        get :show
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'can see Create' do
+        entry = create :entry
+        post :create, params: { entry: { word: entry.word }, format: 'js'}
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'admin' do
+      before :each do
+        @admin = create :admin
+        sign_in @admin
+      end
+
+      it 'can see Show' do
+        get :show
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'can see Create' do
+        entry = create :entry
+        post :create, params: { entry: { word: entry.word }, format: 'js'}
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 
-  context 'user' do
-    before(:each) do
-      @user = create :user
-      sign_in @user
-    end
+  describe 'behavior' do
 
-    it 'can see Show' do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-
-    it 'can see Create' do
-      entry = create :entry
-      post :create, params: { entry: { word: entry.word }, format: 'js'}
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  context 'admin' do
-    before :each do
-      @admin = create :admin
-      sign_in @admin
-    end
-
-    it 'can see Show' do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-
-    it 'can see Create' do
-      entry = create :entry
-      post :create, params: { entry: { word: entry.word }, format: 'js'}
-      expect(response).to have_http_status(:success)
-    end
   end
 end
